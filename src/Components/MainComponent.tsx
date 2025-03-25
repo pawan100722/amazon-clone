a27cf58ed7c9504c0ae4bb6b09bbdf8b2ecb4cf4
pawan100722 <pawan100722@gmail.com>
2025-03-25 19:49:32 +0530

    added selection header

--- src/Components/MainComponent.tsx
+++ src/Components/MainComponent.tsx
@@ -1,10 +1,12 @@
 import '../Styles/MainComponent.css'
 import { Header } from './header.tsx';
+import { SelectionHeader } from './SelectionHeader.tsx';
 
 export const MainComponent=()=>{
   return <div className="main-component-container">
     <nav>
       <Header/>
     </nav>
+    <SelectionHeader/>
   </div>
 }
---
+++ src/Components/SelectionHeader.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { CONSTANT } from "../CONSTANTS";
+import '../Styles/SelectionHeader.css'
+
+export const SelectionHeader =()=>{
+  const [selectionOptions] = useState<string[]>(CONSTANT.SELECTION_HEADER_OPTIONS);
+
+
+  return <div className="selection-header-container">
+  {
+    selectionOptions?.map((sel,indx)=><p className="selection-option white-border" key={indx}>{sel}</p>)
+  }
+  </div>
+}
